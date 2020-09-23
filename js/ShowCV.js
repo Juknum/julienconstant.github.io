6b4c6c19315b298f6a6ef982191df80fd4cdf385
Julien <Juknum@users.noreply.github.com>
2020-09-23 15:47:37 +0200

    add close button

--- js/ShowCV.js
+++ js/ShowCV.js
@@ -2,10 +2,18 @@ function ShowCV(){
 	var divOld = document.getElementById("containerOld");
 	var divNew = document.getElementById("containerNew");
 
-	divOld.style.display = 'hidden';
+	divOld.style.display = 'none';
 	divNew.style.display = 'block';
 
 	fetch('https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/sources/cv.html')
   .then(data => data.text())
-  .then(html => divNew.innerHTML = html);
+  .then(html => divNew.innerHTML = '<a class="btn btn-dark" onclick="HideCV()"><i class="fas fa-times"></i>Fermer</a> <br>' + html);
+}
+
+function HideCV(){
+	var divOld = document.getElementById("containerOld");
+	var divNew = document.getElementById("containerNew");
+	divOld.style.display = 'block';
+	divNew.style.display = 'none';
+	divNew.innerHTML = '';
 }
