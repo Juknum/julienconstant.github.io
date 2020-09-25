6cb5cd7a1c5f1d28d59edaef25518bd4d2e44a77
Julien <Juknum@users.noreply.github.com>
2020-09-25 23:53:21 +0200

    remove async

--- js/LoadPortfolio.js
+++ js/LoadPortfolio.js
@@ -1,34 +1,35 @@
 function LoadPortfolio(lang){
-	
 
-	$.getJSON('https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json', function(data) {
-		var divOld = document.getElementById("containerOld");
-		var divNew = document.getElementById("containerNew");
+	$.ajax({
+	  url: 'https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json',
+	  async: false,
+	  dataType: 'json',
+	  success: function (json) {
+	    var data = json;
+	  }
+	});
 
-		divOld.style.display = 'none';
-		divNew.style.display = 'block';
-	
-		var html = '';
-		var eof = '<hr>';
-		const endBtn = '<hr> <a class="btn btn-danger" onclick="Hide()"><i class="fas fa-times"></i></a>';
+	var divOld = document.getElementById("containerOld");
+	var divNew = document.getElementById("containerNew");
 
-		console.log(data);
+	divOld.style.display = 'none';
+	divNew.style.display = 'block';
+	var html = '';
+	const eof = '<hr>';
+	const endBtn = '<hr> <a class="btn btn-danger" onclick="Hide()"><i class="fas fa-times"></i></a>';
 
-		for (var k = 0; k < data.length; i++){
-			var title = '<h2>' + data[k] + '</h2><br>'
+	console.log(data);
 
-	    for (var i = 0; i < data[k].length; i++) {
-				var obj = data[k][i];
-
-				html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
-
-			}
-
-			divNew.innerHTML += title + html + eof;
-			html = '';
+	for (var k = 0; k < data.length; i++){
+		var title = '<h2>' + data[k] + '</h2><br>'
+    for (var i = 0; i < data[k].length; i++) {
+			var obj = data[k][i];
+			html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
 		}
 
-		divNew.innerHTML += endBtn;
+		divNew.innerHTML += title + html + eof;
+		html = '';
+	}
 
-	});
+	divNew.innerHTML += endBtn;
 }
