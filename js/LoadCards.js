92ebbcbf4848a11c18051aeb46c6b3b7052acff3
Julien <Juknum@users.noreply.github.com>
2020-09-25 22:36:25 +0200

    Update LoadCards.js

--- js/LoadCards.js
+++ js/LoadCards.js
@@ -9,27 +9,10 @@ function LoadCards(IDcategory){
     for (var i = 0; i < data[IDcategory].length; i++) {
 			var obj = data[IDcategory][i];
 
-			var card       = document.createElement('div').classList.add('card');
-			var imgDiv     = document.createElement('div').classList.add('card-image','bg-semi-dark');
-			var img        = document.createElement('img')
-			var contentDiv = document.createElement('div').classList.add('card-cotent','bg-dark');
-			var h2         = document.createElement('h2').classList.add('card-title');
-			var p          = document.createElement('p').classList.add('card-text');
-			var btn        = document.createElement('a').classList.add('btn-portfolio','card-btn');
-			
-			cards.appendChild(card);
-			card.appendChild(imgDiv)
-			card.appendChild(contentDiv);
-			contentDiv.appendChild(h2)
-			contentDiv.appendChild(p)
-			contentDiv.appendChild(btn);
-			imgDiv.appendChild(img);
+			var html = '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
 
-			p.innerHTML   = obj.description;
-			h2.innerHTML  = obj.title;
-			img.src       = obj.image;
-			btn.innerHTML = 'En savoir plus';
-			btn.href      = obj.link;
+			cards.innerHTML += html;
+
 		}
 	});
 }
