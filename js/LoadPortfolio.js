6524eaa028d445a7c18e661c26cb8fe5d0f2da8e
Julien <Juknum@users.noreply.github.com>
2020-09-25 23:39:32 +0200

    Update LoadPortfolio.js

--- js/LoadPortfolio.js
+++ js/LoadPortfolio.js
@@ -1,8 +1,14 @@
 function LoadPortfolio(lang){
+	var divOld = document.getElementById("containerOld");
+	var divNew = document.getElementById("containerNew");
+
+	divOld.style.display = 'none';
+	divNew.style.display = 'block';
 
 	$.getJSON('https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json', function(data) {
 
 		var containerNew = document.getElementById('containerNew');
+		var html = '';
 		var eof = '<hr>';
 
 		console.log(data);
@@ -13,7 +19,7 @@ function LoadPortfolio(lang){
 	    for (var i = 0; i < data[k].length; i++) {
 				var obj = data[k][i];
 
-				var html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
+				html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
 
 			}
 
