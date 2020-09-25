bf983f0d7b419621fe51eb4da1f4cb3ea9034b17
Julien <Juknum@users.noreply.github.com>
2020-09-25 13:02:44 +0200

    hmm

--- js/LoadCards.js
+++ js/LoadCards.js
@@ -1,43 +1,33 @@
 function LoadCards(IDcategory){
-	var requestURL = 'https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json';
-	var request = new XMLHttpRequest();
-	request.open('GET', requestURL);
-	request.responseType = 'json';
-	request.send();
 
-	request.onload = function() {
-		var json = request.response;
-		var cards = document.getElementById(IDcategory);
+	var json = JSON.parse(data);
+	
+	for (var i = 0; i < json[IDcategory].length; i++) {
+		var obj = json[IDcategory][i];
 
-		for (var i = 0; i < json[IDcategory].length; i++) {
+		var card = document.createElement('div').classList.add('card');
+		var imgDiv = document.createElement('div').classList.add('card-image bg-semi-dark');
+		var img = document.createElement('img')
+		img.src = obj.image;
 
-			var obj = json[IDcategory][i];
+		imgDiv.appendChild(img);
+		card.appendChild(imgDiv);
 
-			var card = document.createElement('div').classList.add('card');
-			var imgDiv = document.createElement('div').classList.add('card-image bg-semi-dark');
-			var img = document.createElement('img')
-			img.src = obj.image;
+		var contentDiv = document.createElement('div').classList.add('card-cotent bg-dark');
 
-			imgDiv.appendChild(img);
-			card.appendChild(imgDiv);
+		var h2 = document.createElement('h2').classList.add('card-title');
+		h2.innerHTML = obj.title;
 
-			var contentDiv = document.createElement('div').classList.add('card-cotent bg-dark');
+		var p = document.createElement('p').classList.add('card-text');
+		p.innerHTML = obj.description;
 
-			var h2 = document.createElement('h2').classList.add('card-title');
-			h2.innerHTML = obj.title;
+		var btn = document.createElement('a').classList.add('btn-portfolio card-btn');
+		btn.innerHTML = 'En savoir plus';
+		btn.href = json.link;
 
-			var p = document.createElement('p').classList.add('card-text');
-			p.innerHTML = obj.description;
+		contentDiv.appendChild(h2).appendChild(p).appendChild(btn);
+		card.appendChild(imgDiv).appendChild(contentDiv);
 
-			var btn = document.createElement('a').classList.add('btn-portfolio card-btn');
-			btn.innerHTML = 'En savoir plus';
-			btn.href = json.link;
-
-			contentDiv.appendChild(h2).appendChild(p).appendChild(btn);
-			card.appendChild(imgDiv).appendChild(contentDiv);
-
-			cards.appendChild(card);
-		}
-		
+		cards.appendChild(card);
 	}
 }
