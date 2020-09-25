f7a6acd252b3f51f14ad9d735fdddd381605be35
Julien <Juknum@users.noreply.github.com>
2020-09-25 21:53:01 +0200

    revamp js

--- js/LoadCards.js
+++ js/LoadCards.js
@@ -1,33 +1,33 @@
 function LoadCards(IDcategory){
 
-	var json = JSON.parse(data);
-	
-	for (var i = 0; i < json[IDcategory].length; i++) {
-		var obj = json[IDcategory][i];
+	$.getJSON('https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json', function(data) {
+    for (var i = 0; i < data[IDcategory].length; i++) {
+			var obj = data[IDcategory][i];
 
-		var card = document.createElement('div').classList.add('card');
-		var imgDiv = document.createElement('div').classList.add('card-image bg-semi-dark');
-		var img = document.createElement('img')
-		img.src = obj.image;
+			var card = document.createElement('div').classList.add('card');
+			var imgDiv = document.createElement('div').classList.add('card-image bg-semi-dark');
+			var img = document.createElement('img')
+			img.src = obj.image;
 
-		imgDiv.appendChild(img);
-		card.appendChild(imgDiv);
+			imgDiv.appendChild(img);
+			card.appendChild(imgDiv);
 
-		var contentDiv = document.createElement('div').classList.add('card-cotent bg-dark');
+			var contentDiv = document.createElement('div').classList.add('card-cotent bg-dark');
 
-		var h2 = document.createElement('h2').classList.add('card-title');
-		h2.innerHTML = obj.title;
+			var h2 = document.createElement('h2').classList.add('card-title');
+			h2.innerHTML = obj.title;
 
-		var p = document.createElement('p').classList.add('card-text');
-		p.innerHTML = obj.description;
+			var p = document.createElement('p').classList.add('card-text');
+			p.innerHTML = obj.description;
 
-		var btn = document.createElement('a').classList.add('btn-portfolio card-btn');
-		btn.innerHTML = 'En savoir plus';
-		btn.href = json.link;
+			var btn = document.createElement('a').classList.add('btn-portfolio card-btn');
+			btn.innerHTML = 'En savoir plus';
+			btn.href = obj.link;
 
-		contentDiv.appendChild(h2).appendChild(p).appendChild(btn);
-		card.appendChild(imgDiv).appendChild(contentDiv);
+			contentDiv.appendChild(h2).appendChild(p).appendChild(btn);
+			card.appendChild(imgDiv).appendChild(contentDiv);
 
-		cards.appendChild(card);
-	}
+			cards.appendChild(card);
+		}
+	});
 }
