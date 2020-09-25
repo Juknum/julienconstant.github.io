bd861ea6de1b7a89d28b02795893f3860078c6e0
Julien <Juknum@users.noreply.github.com>
2020-09-25 22:07:16 +0200

    Update LoadCards.js

--- js/LoadCards.js
+++ js/LoadCards.js
@@ -3,20 +3,19 @@ function LoadCards(IDcategory){
 	$.getJSON('https://raw.githubusercontent.com/Juknum/julienconstant.github.io/master/data/portfolio.json', function(data) {
 
 		console.log(data);
-		console.log(data[IDcategory]);
 
     for (var i = 0; i < data[IDcategory].length; i++) {
 			var obj = data[IDcategory][i];
 
-			var card = document.createElement('div').classList.add('card');
-			var imgDiv = document.createElement('div').classList.add('card-image bg-semi-dark');
-			var img = document.createElement('img')
+			var card   = document.createElement('div').classList.add('card');
+			var imgDiv = document.createElement('div').classList.add('card-image','bg-semi-dark');
+			var img    = document.createElement('img')
 			img.src = obj.image;
 
 			imgDiv.appendChild(img);
 			card.appendChild(imgDiv);
 
-			var contentDiv = document.createElement('div').classList.add('card-cotent bg-dark');
+			var contentDiv = document.createElement('div').classList.add('card-cotent','bg-dark');
 
 			var h2 = document.createElement('h2').classList.add('card-title');
 			h2.innerHTML = obj.title;
@@ -24,7 +23,7 @@ function LoadCards(IDcategory){
 			var p = document.createElement('p').classList.add('card-text');
 			p.innerHTML = obj.description;
 
-			var btn = document.createElement('a').classList.add('btn-portfolio card-btn');
+			var btn = document.createElement('a').classList.add('btn-portfolio','card-btn');
 			btn.innerHTML = 'En savoir plus';
 			btn.href = obj.link;
 
