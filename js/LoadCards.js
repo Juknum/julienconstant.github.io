630770a1975d6907f1785556677c462f70356196
Julien <Juknum@users.noreply.github.com>
2020-09-25 22:21:32 +0200

    fix order again

--- js/LoadCards.js
+++ js/LoadCards.js
@@ -7,29 +7,24 @@ function LoadCards(IDcategory){
     for (var i = 0; i < data[IDcategory].length; i++) {
 			var obj = data[IDcategory][i];
 
-			var card   = document.createElement('div').classList.add('card');
-			var imgDiv = document.createElement('div').classList.add('card-image','bg-semi-dark');
-			var img    = document.createElement('img')
-			img.src = obj.image;
-
-			
-
+			var card       = document.createElement('div').classList.add('card');
+			var imgDiv     = document.createElement('div').classList.add('card-image','bg-semi-dark');
+			var img        = document.createElement('img')
 			var contentDiv = document.createElement('div').classList.add('card-cotent','bg-dark');
-
-			var h2 = document.createElement('h2').classList.add('card-title');
-			h2.innerHTML = obj.title;
-
-			var p = document.createElement('p').classList.add('card-text');
-			p.innerHTML = obj.description;
-
-			var btn = document.createElement('a').classList.add('btn-portfolio','card-btn');
-			btn.innerHTML = 'En savoir plus';
-			btn.href = obj.link;
+			var h2         = document.createElement('h2').classList.add('card-title');
+			var p          = document.createElement('p').classList.add('card-text');
+			var btn        = document.createElement('a').classList.add('btn-portfolio','card-btn');
 			
 			cards.appendChild(card);
 			card.appendChild(imgDiv).appendChild(contentDiv);
 			contentDiv.appendChild(h2).appendChild(p).appendChild(btn);
 			imgDiv.appendChild(img);
+
+			p.innerHTML = obj.description;
+			h2.innerHTML = obj.title;
+			img.src = obj.image;
+			btn.innerHTML = 'En savoir plus';
+			btn.href = obj.link;
 		}
 	});
 }
