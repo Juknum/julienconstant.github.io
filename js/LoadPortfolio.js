e4de1c96094966c6a555b4063acb06c7d71dcf35
Julien <Juknum@users.noreply.github.com>
2020-09-26 10:49:17 +0200

    fix it

--- js/LoadPortfolio.js
+++ js/LoadPortfolio.js
@@ -16,25 +16,35 @@ function LoadPortfolio(lang){
 
 	divOld.style.display = 'none';
 	divNew.style.display = 'block';
-	var html = '';
-	const eof = '<hr>';
+	var   html   = '';
+	const eof    = '<hr>';
 	const endBtn = '<hr> <a class="btn btn-danger" onclick="Hide()"><i class="fas fa-times"></i></a>';
 
 	console.log('json :',data);
 
-	for (var k = 0; k < 3; k++){
-		var title = '<h2>' + data[k] + '</h2><br>'
+	var i = 0;
+  var obj = '';
 
-        console.log(k,data.length);
-		
-    for (var i = 0; i < 4; i++) {
-			var obj = data[k][i];
-			console.log('data[',k,'] : ',obj);
-			html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
-		}
+	divNew.innerHTML = '<h2>Projet Personnels:</h2><br>';
+	for(var i = 0; i < data.pp.length; i++) {
+		obj = data.pp[i];
+		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
+	}
 
-		divNew.innerHTML += title + html + eof;
-		html = '';
+	divNew.innerHTML += html + '<hr><h2>Projet Scolaires:</h2><br>';
+	html = '';
+
+	for(var i = 0; i < data.ps.length; i++) {
+		obj = data.ps[i];
+		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
+	}
+
+	divNew.innerHTML += html + '<hr><h2>Projet Communautaires:</h2><br>';
+	html = '';
+
+	for(var i = 0; i < data.pc.length; i++) {
+		obj = data.pc[i];
+		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
 	}
 
 	divNew.innerHTML += endBtn;
