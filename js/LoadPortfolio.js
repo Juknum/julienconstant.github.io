38d17b06333998a07f727a27bb5455a65cd6603a
Julien <Juknum@users.noreply.github.com>
2020-09-26 10:54:35 +0200

    remove console logs & add missing html

--- js/LoadPortfolio.js
+++ js/LoadPortfolio.js
@@ -16,11 +16,10 @@ function LoadPortfolio(lang){
 
 	divOld.style.display = 'none';
 	divNew.style.display = 'block';
-	var   html   = '';
-	const eof    = '<hr>';
+	var   html   = '<ul class="cards">';
 	const endBtn = '<hr> <a class="btn btn-danger" onclick="Hide()"><i class="fas fa-times"></i></a>';
 
-	console.log('json :',data);
+	//console.log('json :',data);
 
 	var i = 0;
   var obj = '';
@@ -31,21 +30,21 @@ function LoadPortfolio(lang){
 		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
 	}
 
-	divNew.innerHTML += html + '<hr><h2>Projet Scolaires:</h2><br>';
-	html = '';
+	divNew.innerHTML += html + '</ul><hr><h2>Projet Scolaires:</h2><br>';
+	html = '<ul class="cards">';
 
 	for(var i = 0; i < data.ps.length; i++) {
 		obj = data.ps[i];
 		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
 	}
 
-	divNew.innerHTML += html + '<hr><h2>Projet Communautaires:</h2><br>';
-	html = '';
+	divNew.innerHTML += html + '</ul><hr><h2>Projet Communautaires:</h2><br>';
+	html = '<ul class="cards">';
 
 	for(var i = 0; i < data.pc.length; i++) {
 		obj = data.pc[i];
 		html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>';
 	}
 
-	divNew.innerHTML += endBtn;
+	divNew.innerHTML += '</ul>' + endBtn;
 }
