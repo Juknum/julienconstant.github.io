a0194d53b0cc1079dc4ae59dbdfd19ce4038aea7
Julien <Juknum@users.noreply.github.com>
2020-09-26 17:29:26 +0200

    fix it

--- js/Background.js
+++ js/Background.js
@@ -14,19 +14,21 @@ function LoadBackground(value) {
 		var webm = document.getElementById('video-webm');
 		var poster = document.getElementById('poster');
 
+		cookieBackground = parseInt(cookieBac, 10);
+
 		console.log(cookieBackground);
 		cookieBackground += value;
 		console.log(cookieBackground);
 
 		// Default background :
-		if (cookieBackground == 0) {
-			mp4.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/248954cba9bc08b6e16c676f5c1814ff823af907.mp4');
-			webm.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/4d466f77edf3265a253fba79d47bc91a37e34920.webm');
-			poster.setAttribute('poster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/da538bac62ee5e233cd82bb75b0d3c47ba0cdc03.jpg');
+		if (cookieBackground === 0) {
+			mp4.setAttribute('src', 'wow');
+			webm.setAttribute('src', 'wop');
+			poster.setAttribute('poster', 'ima a poster lmao');
 		}
 
 		// New one : 
-		if (cookieBackground == 1) {
+		if (cookieBackground === 1) {
 			mp4.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/248954cba9bc08b6e16c676f5c1814ff823af907.mp4');
 			webm.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/4d466f77edf3265a253fba79d47bc91a37e34920.webm');
 			poster.setAttribute('poster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/da538bac62ee5e233cd82bb75b0d3c47ba0cdc03.jpg');
