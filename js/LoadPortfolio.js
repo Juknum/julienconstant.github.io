f403986b5d8280401b6f2467c8d522af1bf38048
Julien <Juknum@users.noreply.github.com>
2020-09-26 10:09:10 +0200

    test

--- js/LoadPortfolio.js
+++ js/LoadPortfolio.js
@@ -26,6 +26,7 @@ function LoadPortfolio(lang){
 		var title = '<h2>' + data[k] + '</h2><br>'
     for (var i = 0; i < data[k].length; i++) {
 			var obj = data[k][i];
+			console.log(obj);
 			html += '<li class="cards-item"><div class="card"><div class="card-image bg-semi-dark"><img src="' + obj.image + '"></div><div class="card-content bg-dark"><h2 class="card-title">' + obj.title + '</h2><p class="card-text">' + obj.description + '</p><a href=' + obj.link + ' class="btn-portfolio card-btn">En savoir plus</a></div></div></li>'
 		}
 
