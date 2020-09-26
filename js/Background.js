4ad41550a6038c5c71f9cbe87c5b865234820e4d
Julien <Juknum@users.noreply.github.com>
2020-09-26 17:03:31 +0200

    try to fix

--- js/Background.js
+++ js/Background.js
@@ -15,13 +15,15 @@ function Background(value) {
 		var webm = document.getElementById('video-webm');
 		var poster = document.getElementById('poster');
 
+		console.log(cookieBackground);
 		cookieBackground += value;
+		console.log(cookieBackground);
 
 		// Default background :
 		if (cookieBackground == 0) {
-			mp4.setAttribute('src', '0');
-			webm.setAttribute('src', '0');
-			poster.setAttribute('poster', '0');
+			mp4.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/248954cba9bc08b6e16c676f5c1814ff823af907.mp4');
+			webm.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/4d466f77edf3265a253fba79d47bc91a37e34920.webm');
+			poster.setAttribute('poster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/da538bac62ee5e233cd82bb75b0d3c47ba0cdc03.jpg');
 		}
 
 		// New one : 
