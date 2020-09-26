1f04fa2def9f25e7cca356d97c9c6048a10ad88e
Julien <Juknum@users.noreply.github.com>
2020-09-26 17:43:27 +0200

    Update Background.js

--- js/Background.js
+++ js/Background.js
@@ -10,8 +10,8 @@ function LoadBackground(value) {
 		// If cookie already exist :
 	} else {
 
-		var mp4  = document.getElementById('video-mp4');
-		var webm = document.getElementById('video-webm');
+		var mp4    = document.getElementById('video-mp4');
+		var webm   = document.getElementById('video-webm');
 		var poster = document.getElementById('poster');
 
 		cookieBackground = parseInt(cookieBackground, 10);
@@ -22,20 +22,31 @@ function LoadBackground(value) {
 
 		// Default background :
 		if (cookieBackground === 0) {
-			mp4.setAttribute('src', 'wow');
-			webm.setAttribute('src', 'wop');
-			poster.setAttribute('poster', 'ima a poster lmao');
+			poster.style.display = 'block'
+
+			mp4.setAttribute('src', 'https://www.w3schools.com/howto/rain.mp4');
 		}
 
 		// New one : 
 		if (cookieBackground === 1) {
+			poster.style.display = 'block';
+
 			mp4.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/248954cba9bc08b6e16c676f5c1814ff823af907.mp4');
 			webm.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/4d466f77edf3265a253fba79d47bc91a37e34920.webm');
 			poster.setAttribute('poster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/da538bac62ee5e233cd82bb75b0d3c47ba0cdc03.jpg');
 		}
 
-		if (cookieBackground > 1) cookieBackground = 0;
+		if (cookieBackground === 2) {
+			poster.style.display = 'none';
+			var background = document.getElementById('background');
 
+			background.style.backgroundColor = 'pink';
+
+		}
+
+		if (cookieBackground > 1) cookieBackground = -1;
+
+		poster.play();
 		localStorage.setItem('BackgroundCookieSelectorJC-Juknum',cookieBackground)
 
 	}
