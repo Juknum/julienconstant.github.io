4ae7931b11f6ba3d9100a834777ae94c0bb20536
Julien <Juknum@users.noreply.github.com>
2020-09-29 23:26:48 +0200

    fix it

--- js/Background.js
+++ js/Background.js
@@ -1,4 +1,4 @@
-function SetCookie(name,value,exdays) {
+function setCookie(name,value,exdays) {
 	var d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
 	var expires = "expires=" + d.toGMTString();
