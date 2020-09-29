e258ca7f09beb3e586f0a0702d21f5b741effa50
Julien <Juknum@users.noreply.github.com>
2020-09-29 23:25:24 +0200

    remove video background for colors

--- js/Background.js
+++ js/Background.js
@@ -1,53 +1,40 @@
-function LoadBackground(value) {
-	var cookieBackground = localStorage.getItem('BackgroundCookieSelectorJC-Juknum');
+function SetCookie(name,value,exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	var expires = "expires=" + d.toGMTString();
+	document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
 
-	// If cookie doesn't exist :
-	if (cookieBackground === undefined) {
+function getCookie(name) {
+	name += "=";
+	var decodedCookie = decodedURIComponent(document.cookie);
+	var ca = decodedCookie.split(";");
 
-		localStorage.setItem('BackgroundCookieSelectorJC-Juknum',0);
-		LoadBackground(0);
-
-		// If cookie already exist :
-	} else {
-
-		var mp4    = document.getElementById('video-mp4');
-		var webm   = document.getElementById('video-webm');
-		var poster = document.getElementById('poster');
-
-		cookieBackground = parseInt(cookieBackground, 10);
-
-		console.log(cookieBackground);
-		cookieBackground += value;
-		console.log(cookieBackground);
-
-		// Default background :
-		if (cookieBackground === 0) {
-			poster.style.display = 'block'
-
-			mp4.setAttribute('src', 'https://www.w3schools.com/howto/rain.mp4');
+	for (var i = 0; i < ca.length; i++){
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
 		}
-
-		// New one : 
-		if (cookieBackground === 1) {
-			poster.style.display = 'block';
-
-			mp4.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/248954cba9bc08b6e16c676f5c1814ff823af907.mp4');
-			webm.setAttribute('src', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/4d466f77edf3265a253fba79d47bc91a37e34920.webm');
-			poster.setAttribute('poster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/1263950/da538bac62ee5e233cd82bb75b0d3c47ba0cdc03.jpg');
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
 		}
-
-		if (cookieBackground === 2) {
-			poster.style.display = 'none';
-			var background = document.getElementById('background');
-			background.style.backgroundColor = 'pink';
-
-		}
-
-		if (cookieBackground > 1) cookieBackground = -1;
-
-		poster.play();
-		localStorage.setItem('BackgroundCookieSelectorJC-Juknum',cookieBackground)
-
 	}
 
+	return "";
+}
+
+function loadColor() {
+	var color = getCookie("BACKGROUNDCOLOR");
+	if (color != "") {
+		document.getElementById("background").style.backgroundColor = '#' + color;
+	}
+}
+
+function checkColor() {
+
+	color = prompt("Entrez une couleur héxadécimale :","");
+	if (color != "" && color != null) {
+		setCookie("BACKGROUNDCOLOR",color,365);
+		document.getElementById("background").style.backgroundColor = '#' + color;
+	}
 }
